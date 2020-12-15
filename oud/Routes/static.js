31461f93d92211315d8ef82183dc253c4c516b6a
Sjoerd Bolten <sjoerdabolten@gmail.com>
2020-12-15 04:38:39 +0100

    Remove legacy code

--- oud/Routes/static.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-
-const router = Router();
-
-router.get('/*', (req, res) => res.redirect('https://github.com/netlob/spotistats-api'));
-
-module.exports = router;
