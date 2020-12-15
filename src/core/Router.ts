433d8f1b0eeade73d6858271dfb5000aec69c0c3
Sjoerd Bolten <sjoerdabolten@gmail.com>
2020-12-15 04:30:04 +0100

    Removed legacy routes

--- src/core/Router.ts
+++ src/core/Router.ts
@@ -3,8 +3,6 @@ import authRouter from "../routers/auth";
 
 import { infoLogger } from "../misc/Logger";
 
-// import { spotifyCallback, getUserStreams, getStreams, getSongStreams, getUserHistoryInfo, getUserSong, getUserArtist } from "../Routes";
-
 export default async (): Promise<void> => {
   const app = express();
 
@@ -13,37 +11,6 @@ export default async (): Promise<void> => {
   );
 
   app.use(authRouter);
-
-  // app.get("/api/streams", async (req, res) => await getStreams(req, res));
-
-  // // app.get("/api/user/:userID", async (req, res) => await getUserInfo(req, res));
-
-  // app.get(
-  //   "/api/user/:userID/streams",
-  //   async (req, res) => await getUserStreams(req, res)
-  // );
-
-  // app.get(
-  //   "/api/user/:userID/dates",
-  //   async (req, res) => await getUserHistoryInfo(req, res)
-  // );
-
-  // // app.get("/api/song/:songID", async (req, res) => await getUserStreams(req, res));
-
-  // app.get(
-  //   "/api/song/:songID/streams/:userID",
-  //   async (req, res) => await getSongStreams(req, res)
-  // );
-
-  // app.get(
-  //   "/api/user/:userID/artist/:artistID",
-  //   async (req, res) => await getUserArtist(req, res)
-  // );
-
-  // app.get(
-  //   "/api/user/:userID/song/:songID",
-  //   async (req, res) => await getUserSong(req, res)
-  // );
 
   app.listen("8123", () => infoLogger("Listening on http://localhost:8123"));
 };
