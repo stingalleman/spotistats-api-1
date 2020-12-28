76f823c6d007b22ee930150e92ffffa8ab7e1c2e
Sjoerd Bolten <sjoerdabolten@gmail.com>
2020-12-28 22:48:25 +0100

    Add API endpoints

--- src/routers/api.ts
+++ src/routers/api.ts
@@ -1,73 +1,113 @@
 import { Router } from "express";
-import { getManager, getRepository } from "typeorm";
+import { getManager, LessThan, MoreThan } from "typeorm";
 import { UserTrack } from "../entities";
 
 const apiRouter = Router();
 
-async function getTop(params) {
-  if (params.type === "artists") {
-    return getManager().query(
-      `
+async function getTop(req) {
+  try {
+    const limit = !req.query.limit ? 10 : req.query.limit;
+
+    if (req.params.type === "artists") {
+      return getManager().query(
+        `
       SELECT
-        SUM(count), "artistName"
+        COUNT("artistName"), "artistName"
       FROM
         usertracks
       WHERE
         "userId" = $1
       GROUP BY "artistName"
-      ORDER BY SUM(count) DESC
-      LIMIT 10;
+      ORDER BY COUNT("artistName") DESC
+      LIMIT $2;
       `,
-      [params.userId]
-    );
-  } else {
-    const query = {
-      where: {
-        user: {
-          id: params.userId,
+        [req.params.userId, limit]
+      );
+    } else {
+      const query = {
+        where: {
+          user: {
+            id: req.params.userId,
+          },
         },
-      },
-      skip: 0,
-      take: 10,
-      cache: true,
+        skip: 0,
+        take: limit,
+        cache: true,
+      };
+      query["order"] = {
+        count: "DESC",
+      };
+
+      const top = await UserTrack.find(query);
+
+      return {
+        length: top.length,
+        data: top,
+      };
+    }
+  } catch (e) {
+    return {
+      error: e.toString(),
     };
-    query["order"] = {
-      count: "DESC",
-    };
-    return await UserTrack.find(query);
   }
 }
 
-async function getStreams(params) {
-  const query = {
-    user: {
-      id: params.userId,
-    },
-  };
+async function getStreams(req) {
+  try {
+    const query = {
+      user: {
+        id: req.params.userId,
+      },
+    };
 
-  if (params.artistName !== undefined) {
-    query["artistName"] = encodeURI(params.artistName);
+    if (req.params.artistName !== undefined) {
+      query["artistName"] = encodeURI(req.params.artistName);
+    }
+
+    if (req.params.trackName !== undefined) {
+      query["trackName"] = encodeURI(req.params.trackName);
+    }
+
+    if (req.query.before !== undefined) {
+      query["endTime"] = LessThan(new Date(req.query.before));
+    }
+
+    if (req.query.after !== undefined) {
+      query["endTime"] = MoreThan(new Date(req.query.after));
+    }
+
+    const streams = await UserTrack.find(query);
+
+    return {
+      length: streams.length,
+      data: streams,
+    };
+  } catch (e) {
+    return {
+      error: e.toString(),
+    };
   }
-
-  if (params.trackName !== undefined) {
-    query["trackName"] = encodeURI(params.trackName);
-  }
-
-  return await UserTrack.find(query);
 }
 
 apiRouter.get("/v1/api/user/:userId/top/:type", async (req, res) => {
-  res.send(await getTop(req.params)).end();
+  res.send(await getTop(req)).end();
 });
 
-apiRouter.get("/v1/api/user/:userId/artist/:artistName", async (req, res) => {
-  res.send(await getStreams(req.params)).end();
+apiRouter.get("/v1/api/user/:userId/streams", async (req, res) => {
+  res.send(await getStreams(req)).end();
 });
 
 apiRouter.get(
-  "/v1/api/user/:userId/artist/:artistName/track/:trackName",
+  "/v1/api/user/:userId/streams/artist/:artistName",
   async (req, res) => {
-    res.send(await getStreams(req.params)).end();
+    res.send(await getStreams(req)).end();
+  }
+);
+
+apiRouter.get(
+  "/v1/api/user/:userId/streams/artist/:artistName/track/:trackName",
+  async (req, res) => {
+    res.send(await getStreams(req)).end();
   }
 );
 
